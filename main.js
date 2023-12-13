bc453be756a549a6ef92aadad1ef5f0fec59f930
Yegon Kiprotich <meshcode75@gmail.com>
2023-12-13 20:27:54 +0300

    just updated the navigation to make it responsive, I want to work on it now until I can deliver and host to vercel so that I can show Rachel.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //toggle and untoggle service display on hover
-var dropdownbutton= document.getElementById("dropDownButton");
+// var dropdownbutton= document.getElementById("dropDownButton");
 
-var dropdowncontent=document.getElementById("dropDownContent");
+// var dropdowncontent=document.getElementById("dropDownContent");
 
-dropdownbutton.addEventListener("mouseover", ()=>{
-    dropdowncontent.style.display="block";
-    dropdowncontent.style.animatae="all 2s ease";
-});
-dropdownbutton.addEventListener("mouseout", ()=>{
-    dropdowncontent.style.display="none";
-})
+// dropdownbutton.addEventListener("mouseover", ()=>{
+//     dropdowncontent.style.display="none";
+//     dropdowncontent.style.animatae="all 2s ease";
+// });
+// dropdownbutton.addEventListener("mouseout", ()=>{
+//     dropdowncontent.style.display="none";
+// })
